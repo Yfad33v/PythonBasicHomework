7d36f3948491eb683d2d2365c62c2294a7486fba
Yfad33v <geoscorp@yandex.ru>
2021-12-14 21:35:36 +0300

    Add Corrected HomeWork_1

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,27 +3,25 @@
 def power_numbers(*args: int):
    return [i**2 for i in args if isinstance(i, int)]
 
-def is_prime(a):
-    k = 0
-    for i in range(2, a // 2+1):
-        if (a % i == 0):
-            k = k+1
-        if (k <= 0):
-            return True
+def is_prime(k):
+    if k > 1:
+        for i in range(2, int(k / 2) + 1):
+            if (k % i) == 0:
+                return False
         else:
-            return False
-
+            return True
+    else:
+        return False
 
 # filter types
 ODD = "odd" #нечетный
 EVEN = "even"  # четный
 PRIME = "prime" # простое число
 
-def filter_numbers(*args, check):
-    print("args= ", args,"check = ", check)
+def filter_numbers(args,check):
     if check == "even":
         return list(filter(lambda x: x % 2 == 0, args))
     if check == "odd":
-        return list(filter(lambda x: x % 2 !=0, args))
+        return list(filter(lambda x: x % 2 != 0, args))
     if check == "prime":
         return list(filter(is_prime, args))
